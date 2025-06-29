0643b7c9d28aad64b7ffca5509276fe1cb5fa53b
Raymond Akpusugh <akpusughsesughter@gmail.com>
2025-06-29 21:46:52 +0100

    Done

---
+++ Dockerfile/Main.java
@@ -0,0 +1,353 @@
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    String name = "John";
+    System.out.println(name);
+    int x = 3, y = 4;
+    System.out.println(x * y);
+    String studentName ="John Sams";
+    int studentId = 001;
+    int studentAge = 20;
+    double studentFee = 75.75d;
+    char studentGrade = 'B';
+
+    System.out.println("Student Name: " + studentName);
+    System.out.println("Student Id: " + studentId);
+    System.out.println("Student Age: " + studentAge);
+    System.out.println("Student Fee: " + studentFee);
+    System.out.println("Student Grade: " + studentGrade);
+
+    //create integer variabels
+    int length = 4;
+    int width = 6;
+    int area;
+
+    area = length * width;
+
+    System.out.println("Length is: " + length);
+    System.out.println("Width is: " + width);
+    System.out.println("Area of a rectangle is:" + area);
+    //create a double variable
+    double d1 = 35e3f;
+    float f1 = 12e4f;
+    System.out.println("Double value: " + d1);
+    System.out.println("Float value: " + f1);
+
+    //create a boolean variable
+    boolean isJavaFun = true;
+    boolean isFishTasty = false;
+    System.out.println("Is Java fun? " + isJavaFun);
+    System.out.println("Is fish tasty? " + isFishTasty);
+
+    //create a character variable
+    char grade = 'A';
+    System.out.println("Grade: " + grade);
+    //create variables of different data types
+    int items = 50;
+    double costPerItem = 9.99d;
+    double totalCost = items * costPerItem;
+    char currency = '$';
+    boolean inStock = true;
+
+    // Print the variables
+    System.out.println("Number of items: " + items);
+    System.out.println("Cost per item: " + currency + costPerItem);
+    System.out.println("Total cost: " + currency + totalCost);
+    System.out.println("In stock: " + inStock);
+
+    //Widening Casting 
+    int myInt = 9;
+    double myDouble = myInt;
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+
+    //Narrowing Casting
+    double myDouble2 = 9.78d; 
+    int myInt2 = (int) myDouble2; // Manual casting : double to int
+    System.out.println(myDouble2);
+    System.out.println(myInt2); 
+
+    /*Real-Life example: here is a real life example of casting where we creaye a program to calculate the percentage of a user"s score in relation to the maximum score in a game 
+     * 
+     * we use type casting to make sure that the result is a floating-point value, rather than an integer.
+     */
+    // Set the maximum possible score in the game to 500
+    int maxScore = 500;
+
+    // The actual score of the user
+
+    int userScore = 423;
+    /*Calculate the Percentage of the user's score in relation to the maximum available score. Convert userScore to make sure that the division is accurate */
+    Float percentage = (float) userScore / maxScore * 100.0f;
+    char percentageSymbol = '%';
+    // Print the result
+    System.out.println("User's percentage is " + percentage + percentageSymbol);
+
+    /*Java Operators: Operators are used to perform operations on variables and values.
+     * in this example, we use the + operator to add together two values 
+     */
+
+     //int a = 100 + 50; // Assign the value 150 to a
+     
+     /*Althoug the + operator is often used to add together two values, like in the example above, it can also be used to add together a variable and a value, or a variable and another variable */
+
+     int sum1 = 100 + 50;
+     int sum2 = sum1 + 250;
+     int sum3 = sum2 + sum2;
+     System.out.println("Sum1: " + sum1);
+      System.out.println("Sum2: " + sum2);
+      System.out.println("Sum3: " + sum3);
+
+      /*
+       * Java Arithmetic Operators: Arithmetic operators are used to perform basic mathematical operations such as addition, subtraction, multiplication, and division.
+       * operators
+       *        eg.
+       *                  same as
+       * =	   x = 5	    x = 5	
+        +=     x += 3	    x = x + 3	
+        -=     x -= 3	    x = x - 3	
+        *=	   x *= 3	    x = x * 3	
+        /=	   x /= 3   	x = x / 3	
+        %=	   x %= 3	    x = x % 3	
+        &=   	 x &= 3	    x = x & 3	
+        |=	   x |= 3	    x = x | 3	
+        ^=	   x ^= 3    	x = x ^ 3	
+        >>=	   x >>= 3	  x = x >> 3	
+        <<=	   x <<= 3	  x = x << 3
+       */
+
+       String txt = "ASHRHDJHFSHGFYHBFUUYGUSGUIYGRYGFUGY";
+       System.out.println("The length of the txt string is: " + txt.length());
+
+       String txt2 = "Hello World";
+       System.out.println(txt2.toUpperCase());
+       System.out.println(txt2.toLowerCase());
+
+       //Finding a character in a string
+       String txt3 = "Please can locate where 'locate' occurs!";
+       System.out.println(txt3.indexOf("locate"));
+
+       //concatination of strings
+       String firstName = "Mark";
+       String lastName = "Smith";
+       System.out.println(firstName + " " + lastName); 
+       //OR
+       System.out.println(firstName.concat(lastName));
+
+       String txt4 = "We \'are the\' \\so-called\\ \"Vikings\" from the north.";
+       System.out.println(txt4);
+      /*\n	New Line	
+        \r	Carriage Return	
+        \t	Tab	
+        \b	Backspace	
+        \f	Form Feed */
+        String txt5 = "We are the \n so-called \"Vikings\" from the north.";
+        System.out.println(txt5);
+
+        /* Java Math: The Math.max(x,y) method can be used to find the highest value of X and Y 
+         * Example:
+         */
+        Math.max(5, 10); // returns 10
+
+        /* Java Math: The Math.min(x,y) method can be used to find the lowest value of X and Y 
+         * Example:
+         */
+        Math.min(5, 10); // returns 5
+        /* Java Math: The Math.sqrt(x) method can be used to find the square root of X 
+         * Example:
+         */
+        Math.sqrt(64); // returns 8.0
+        /* Java Math: The Math.abs(x) method can be used to find the absolute (positive) value of X 
+         * Example:
+         */
+        double find = Math.abs(-4.7); // returns 4.7
+        System.out.println(find);
+        /* Java Math: The Math.random() method can be used to generate a random number between 0.0 and 1.0 
+         * Example: 
+         */ int randomNum = (int)(Math.random() * 101);
+         System.out.println("Random Interger between 0 and 100: " + randomNum);
+        double randomValue = Math.random(); // returns a random number between 0.0 and 1.0
+        System.out.println("Random value: " + randomValue);
+        
+
+        // boolean expressinos
+        int a = 7;
+        int b = 5;
+        System.out.println(a>b);
+
+        int e =13;
+        System.out.println(e == 13);
+
+        System.out.println(10 == 17);
+
+        int myAge = 25;
+        int votingAge = 18;
+        System.out.println(myAge >= votingAge);
+        /* An even better approach (since we are on a roll now), would be to wrap the code above in an if...else statement, so we can perform different actions depending on the result: */
+        int myAge2 = 5;
+        int votingAge2 = 18;
+        if (myAge2 >= votingAge2) {
+            System.out.println("You are old enough to vote.");
+        } else {
+            System.out.println("You are not old enough to vote.");
+        }
+        /*Use if to specify a block of code to be executed, if a specified condition is true
+Use else to specify a block of code to be executed, if the same condition is false
+Use else if to specify a new condition to test, if the first condition is false
+Use switch to specify many alternative blocks of code to be executed */
+        int num1 = 50;
+        int num2 = 45;
+        if (num1 > num2) {
+          System.out.println(num1 + " is the bigger number");
+        } else {
+          System.out.println(num2 + " is the bigger number");
+        }
+
+        // The else Statement: Use the else statement to specify a block of code to be executed if the condition is False
+        
+        /*the else if statement: use this statement to specify a new condition if the first condition is fales */
+        int time = 20;
+        if (time < 10) {
+            System.out.println("Good morning.");
+        } else if (time == 20) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+
+        int time2 = 20;
+        String result = (time2 < 30) ? "Good day." : "Good evening.";
+        System.out.println(result);
+
+        int myNum = 5;
+
+        if (myNum % 2 == 0) {
+            System.out.println(myNum + " is an even number.");
+        } else {
+            System.out.println(myNum + " is an odd number.");
+        }
+
+        /*Java Switch statements
+         * Instead of writing many if...else statements, you can use a switch statement to select one of many code blocks to be executed.
+         * A switch statement can have one or more case statements, and the switch expression is evaluated once.
+         * The switch expression is evaluated once.
+The value of the expression is compared with the values of each case.
+If there is a match, the associated block of code is executed.
+The break and default keywords are optional, and will be described later in this chapter
+         */
+        int day = 9;
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("It's the weekend!");
+
+            /*
+             * Java while loop: Loops can execute a block of code as long as a specified condition is true.
+             * loops are handy because they save time, reduce errors and they make code more readable.
+             */
+        }
+        int i = 0;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+            /*
+             * Count downn Example:
+             */
+            int countdown = 3;
+            while (countdown > 0) {
+                System.out.println(countdown);
+                countdown--;
+            }
+            System.out.println("Happy New Year!!");
+        /*
+         * The Do/While Loop
+         * The do/while loop is a variant of the while loop. this will execute the code block once,
+         * before checking if the condition is true. Then it will reapeat the loop as long as the condition is true.
+         *
+         * The example below uses a do/while loop. The loop will always be executed at least once, even if the condition is false, because the code block is executed before the code block is executed before the condition is tested:
+         */
+            int i2 = 1;
+            do {
+                System.out.println(i2);
+                i2++;
+            }
+            while (i2 < 5);
+
+            int i3 = 10;
+            do {
+                System.out.println(i3);
+                i3++;
+            } 
+            while (i3 < 6);
+        }
+        /* A Real-life eample : To demonstrate a practical example of the while loop conbined with an if else statement, let's say we play a game of Yatzy
+         */
+        int dice = 1;
+
+        while (dice <= 6) { 
+        if (dice < 6) {
+            System.out.println("No Yatzy.");
+        
+         } else {
+            System.out.println("Yatzy");
+         }
+         dice = dice + 1;
+        }
+        /*
+         * Java For Loop 
+         * When you Know Exactly How many times You want to loop Through a block of code , use The For loop instead of a While  Loop :
+         * Statement 1 is executed (one time) before the execution of the code block.
+         * Statement 2 defines the condition for executing the code Block.
+         * Statement 3 is executed (every time) after the code block has been executed.
+         * Print Numbers
+         * The example below will print the numbers 0 to 4:
+         * Example
+         */
+        for (int i4 = 0; i4 < 5; i4++) {
+            System.out.println(i4);
+        }
+
+        for (int w1 = 0; w1 < 10; w1++) {
+            System.out.println("yes");
+        }
+        //FOR loop to print even numbers 
+        for (int i5 = 0; i5 <= 20; i5 = i5+ 2) {
+            System.out.println(i5);
+        }
+        //FOR loop to priint the sum of numbers
+        //this example calculates the sum of numbers from 1 to 5:
+
+        int sum = 0;
+        for (int i6 = 1; i6 <= 5; i6++) {
+            sum = sum + i6; // or sum += i6;
+        } 
+        System.out.println("The sum of numbers from 1 to 5 is: " + sum);
+
+        // countdown2
+        for (int i7 = 5; i7 > 0; i7--) {
+            System.out.println(i7);
+        }
+
+        /*
+         * Nested Loops: It is also possible to Place a loop inside another loop. It"s called a Nested loop.
+         * The "inner loop" will be executed one time for each iteration of the "outer loop":
+         */
+
+         //Outer Loop 
+         for (int i8 = 1; i8 <= 2; i8++) {
+            System.out.println("Outer loop: " + i8);//Executes 2 times
+
+            // Inner loop
+            for (int j = 1; j <= 3; j++) {
+                System.out.println("Inner: " + j); // Executes 6 times (2 * 3)
+            }
+         }
+            
+  }
+}
---
+++ Dockerfile/maincopy.java
@@ -0,0 +1,34 @@
+public class maincopy {
+    public static void main(String[] args){
+        String name = "John";
+        System.out.println("Hello, " + name );
+
+        String countryName = "USA";
+        String studentName = "John Sams";
+        double studentId = 56e4f;
+        int studentAge = 21;
+        double studentFee = 75.75d;
+        char studentGrade = 'B';
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student Id: " + studentId);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Fee: " + studentFee);
+        System.out.println("Student Grade: " + studentGrade);
+        System.out.println("Where do you live? " + countryName);
+        
+        int items1 = 45;
+        double costPerItem = 44.4;
+        double totalCost = items1 * costPerItem;
+        char currency = '$';
+        System.out.println("Items in stock: " + items1);
+        System.out.println("Cost: " + currency + costPerItem );
+        System.out.println("Total cost of Items: " + currency + totalCost);
+        
+
+        int myiint = 56;
+        double mydoo = myiint; 
+        System.out.println(mydoo);
+
+    }
+    
+}
